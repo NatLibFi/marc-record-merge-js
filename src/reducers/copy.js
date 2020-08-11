7404c157b6b21841859c7755d9b5e4472d7570ab
ssilvonen <46997466+ssilvonen@users.noreply.github.com>
2020-08-11 11:56:53 +0300

    finished copy.js

--- src/reducers/copy.js
+++ src/reducers/copy.js
@@ -51,8 +51,8 @@ export default (pattern) => (base, source) => {
     }
     // Jos basessa on jo kenttä, sourcen kenttä kopioidaan uutena basen kentän lisäksi
     // Eli merged.jsonissa voi olla useampi kappale samaa kenttää.
-
     // SourceField käsittelee vuorotellen jokaisen patternin mukaisen kentän sourcesta
+
     const filterMissing = function(sourceField) {
       // Testi 02: Onko identtisiä kontrollikenttiä?
       // If value in sourceField kertoo, että kyseessä on kontrollikenttä,
@@ -66,29 +66,22 @@ export default (pattern) => (base, source) => {
       }
 
       // Jos sourceFieldillä on subfields-osio, kyseessä on datakenttä.
-      // Testi 04: Onko identtisiä datakenttiä? (04:ssä on 2 kpl)
-      // Testi 05: Yksi samanlainen ja yksi erilainen datakenttä
+      // Testi 04: identtiset datakentät basessa ja sourcessa
+      // Testi 05: erilaisia datakenttiä basessa ja sourcessa
       if ('subfields' in sourceField) {
         debug('tarkistetaan datakentät');
         debug(`sourceField datakentät on: ${JSON.stringify(sourceField, undefined, 2)}`);
         return baseFields.some(isIdenticalDataField) === false;
       }
-
-      // Debug(`filterMissingin arvo on: ${JSON.stringify(filterMissing, undefined, 2)}`);
-      // Return filterMissing; // Palauttaa ei-identtiset kentät
+      debug(`filterMissingin arvo on: ${JSON.stringify(filterMissing, undefined, 2)}`);
+      return filterMissing; // Palauttaa ei-identtiset kentät
 
       // Apufunktiot:
-      // Marc-kentän normalisointi
+      // Kontrollikentän normalisointi
       function normalizeControlField(field) {
+        debug(`kontrollikentän field.value on ${field.value}`);
         return field.value.toLowerCase().replace(/\s+/u, '');
       }
-      function normalizeDataField(field) {
-        // Return field.subfields[0].value.toLowerCase().replace(/\s+/u, ''); // tämä toimii mutta vain ekalle indeksille
-        const x = field.subfields.map(subfield => subfield.value.toLowerCase().replace(/\s+/u, '')); // Miksi tämä ei toimi?
-        debug(`tulos on ${x}`);
-        return x;
-      }
-
       // Ovatko normalisoitu base ja source identtisiä?
       // Jos funktio palauttaa true niin ovat, jos false niin eivät
       function isIdenticalControlField(baseField) {
@@ -96,24 +89,32 @@ export default (pattern) => (base, source) => {
         const normalizedSourceField = normalizeControlField(sourceField);
         return normalizedSourceField === normalizedBaseField;
       }
-
-      // Ovatko datakentät identtisiä? tag, ind1, ind2 ja kaikki osakentät
+      // Ovatko datakentät identtisiä? tag, ind1, ind2 ja kaikki osakentät pitää olla identtiset.
       function isIdenticalDataField(baseField) {
-        const normalizedBaseField = normalizeDataField(baseField);
-        const normalizedSourceField = normalizeDataField(sourceField);
         if (sourceField.tag === baseField.tag &&
           sourceField.ind1 === baseField.ind1 &&
           sourceField.ind2 === baseField.ind2 &&
           sourceField.subfields.length === baseField.subfields.length) {
-        // Jos mikään näistä ei toteudu, palauttaa false
-        // Ja tämän sisään tulee subfieldien vertailu
-        // SourceField.subfields[0].code === baseField.subfields[0].code &&
-        // SourceField.subfields[0].value === baseField.subfields[0].value) {
+          // SourceFieldin ja baseFieldin kaikissa subfieldeissä pitää olla samat code ja value
+          // Osakenttien normalisointi
           debug('tarkistetaan datakenttien sisällöt');
-          return normalizedSourceField === normalizedBaseField;
+          return baseField.subfields.every(isIdenticalSubfield);
         }
-      }
+        function normalizeSubfield(subfield) {
+          const x = subfield.value.toLowerCase().replace(/\s+/u, '');
+          debug(`normalizeSubfield: ${x}`);
+          return x;
+        }
+        function isIdenticalSubfield(baseSub) {
+          const normBaseSub = normalizeSubfield(baseSub);
+          return sourceField.subfields.some(sourceSub => {
+            const normSourceSub = normalizeSubfield(sourceSub);
+            return normSourceSub === normBaseSub;
+            });
+        }
+      }; //isIdenticalDatafieldin loppu
     }; // FilterMissingin loppu
+
 
     // MissingFieldsillä etsitään mitkä kentät puuttuvat basesta
     const missingFields = sourceFields.filter(filterMissing);
@@ -123,5 +124,6 @@ export default (pattern) => (base, source) => {
     missingFields.forEach(f => base.insertField(f));
     debug(`base lopussa pitää olla sama kuin merged: ${JSON.stringify(base, undefined, 2)}`);
     return base; // CopyFieldsin pitäisi palauttaa tämä
+
   } // CopyFieldsin loppu
 }; // Export defaultin loppu
